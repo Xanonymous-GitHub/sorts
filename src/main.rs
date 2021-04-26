0eb07b58b0d281c1a3af480e1759e1f6494456d8
Xanonymous <trusaidlin@gmail.com>
2021-04-26 09:55:44 +0800

    insertion sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use rand::prelude::*;
+use colored::Colorize;
+
+mod insertion_sort;
+
+fn get_result_text(success: bool) -> &'static str {
+    if success {
+        "PASSED"
+    } else {
+        "FAILED"
+    }
+}
+
+fn get_rand_arr<const SIZE: usize>(rng: &mut ThreadRng, n: i32) -> [i32; SIZE] {
+    let mut arr = [0; SIZE];
+    for x in &mut arr {
+        *x = rng.gen_range(-n..n);
+    }
+    arr
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let test_array: [i32; 10000] = get_rand_arr(&mut rng, 100000);
+    let mut sorted_test_array = test_array.clone();
+    sorted_test_array.sort();
+
+    // insertion sort
+    let mut unsorted = test_array.clone();
+    let insertion_sort_result = insertion_sort::insertion_sort(&mut unsorted);
+    println!("Insertion sort {}", get_result_text(insertion_sort_result == sorted_test_array).green());
+    assert_eq!(insertion_sort_result, sorted_test_array);
 }
